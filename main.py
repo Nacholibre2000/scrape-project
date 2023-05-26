1f3b375107882817616050531547e24971e6a537
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-05-26 22:08:42 +0000

    trial of scraping

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=-996270488%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRSAM01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f"
+
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
+coursedetails = soup.find_all(text="class=course-details")
+
+print(coursedetails)
