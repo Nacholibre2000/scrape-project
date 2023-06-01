cbb4c50871900b0783e6c2e4d146f3891c1d9876
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-06-01 20:22:31 +0000

    major progress

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=-996270488%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRSAM01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f"
+with open('source.html', 'r', encoding='utf-8') as file:
+    page_content = file.read()
 
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
-#subsection = soup.find_all('h4', class_="c12638") # to find subsection
+# url = "https://www.skolverket.se/undervisning/grundskolan/laroplan-och-kursplaner-for-grundskolan/laroplan-lgr22-for-grundskolan-samt-for-forskoleklassen-och-fritidshemmet?url=-996270488%2Fcompulsorycw%2Fjsp%2Fsubject.htm%3FsubjectCode%3DGRGRSAM01%26tos%3Dgr&sv.url=12.5dfee44715d35a5cdfa219f"
+# page = requests.get(url)
 
-central_content = soup.find_all('section', class_="courses-wrapper")
+soup = BeautifulSoup(page_content, "html.parser")
+results = soup.find('section', class_="courses-wrapper")
 
-for ul in central_content:
-  subsection, central_content = 
-  print(subsection.ul)
-  print(central_content.li)
+grade = results.find('h3', class_="c12638")
+print(grade.string)
+div = results.find_all('div', class_="course-details")
 
-## OLDS
-
-#coursedetails = soup.find_all("div", {"class": "course-details"})
-
-#subsection = soup.find_all('h4', class_="c12638") # to find subsection
-#print(subsection) # to find subsection
-
-
-#<h3 class="c12638"> Är årskurs
-#<h4 class="c12638"> Är subsection
-#<li> Är alla centrala innehåll
-#grades = coursedetails.find_all("h3", {"class": "c12638"})
-
-#print(coursedetails)
-#print(soup)
-
-#grade = soup.find_all('h3', class_="c12638") # to find grade
-# print(grade) # to find grade
+for div_tags in div:
+    ul_tags = div_tags.find_all('ul')
+    for ul in ul_tags:
+        subsection = ul.find_previous_sibling('h4')
+        if subsection:
+            print(subsection.string)
+        else:
+            print("No preceding h4 sibling found for ul.")
+        
+        for li in ul.find_all('li'):
+            print(li.string)
