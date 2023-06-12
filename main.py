68c23787cb76347c062d7cf49ef602193c2223ee
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-06-12 19:13:22 +0000

    all information successfully scraped

--- main.py
+++ main.py
@@ -8,59 +8,48 @@ with open('source.html', 'r', encoding='utf-8') as file:
 # page = requests.get(url)
 
 soup = BeautifulSoup(page_content, "html.parser")
-results_content = soup.find('section', class_="courses-wrapper")
-results_requirements = results_content.find_next_sibling('section')
+result = soup.find('div', class_="content")
 
-div = results_content.find_all('div', class_="course-details")
-# ul = div.find('ul')
-# ul_sibling = ul.find_previous_sibling('h4')
-# print(ul_sibling.string)
-# for child in ul.children:
-#         print(child.string)
+central_contents = result.find('section')
+course_requirements = central_contents.find_next_sibling('section')
 
-for div_tags in div:
-  grade = div_tags.find('h3')
+for central_contents_tags in central_contents.find_all(
+    'div', class_="course-details"):
+  grade = central_contents_tags.find('h3')
   print(grade.string)
-  ul_tags = div_tags.find_all('ul')
+  ul_tags = central_contents_tags.find_all('ul')
   for ul in ul_tags:
     subsection = ul.find_previous_sibling('h4')
     if subsection:
       print(subsection.string)
     else:
       print("No preceding h4 sibling found for ul.")
+    for central_content in ul.find_all('li'):
+      print(central_content.string)
 
-    for li in ul.find_all('li'):
-      print(li.string)
-for
-#   for grade in grades:
-#     print(f"Grade:{grade.text}")
-#   subsection_list = result.find_all('h4', class_="c12638")
-#   for subsection in subsection_list:
-#     print(f"Subsection: {subsection.text}")
-#   central_content_list = result.find_all('li')
-#   for central_content in central_content_list:
-#     print(f"Central content: {central_content.text}")
+for articles in course_requirements.find_all('article'):
+  grade = articles.find('h3')
+  print(grade.string)
+  for paragraph in articles.find('div', class_="course-details").find_all('p'):
+    paragraph_text = paragraph.get_text()
+    sentences = paragraph_text.split('. ')
+    #sentences = list(filter(None, sentences))
+    for sentence in sentences:
+      print(sentence)
 
-#  print(f"Subsection:{subsection.text}")
-#  print(f"Central content:{course_requirement.text}")
+#for central_requirement_tags in central_requirements.find_all('div', class_="course-details")
 
-#print(grades)
-#<h4 class="c12638">Att leva tillsammans</h4>
-#for ul in central_content:
-## OLDS
+#div = result.find_all('div', class_="course-details")
 
-#coursedetails = soup.find_all("div", {"class": "course-details"})
-
-#subsection = soup.find_all('h4', class_="c12638") # to find subsection
-#print(subsection) # to find subsection
-
-#<h3 class="c12638"> Är årskurs
-#<h4 class="c12638"> Är subsection
-#<li> Är alla centrala innehåll
-#grades = coursedetails.find_all("h3", {"class": "c12638"})
-
-#print(coursedetails)
-#print(soup)
-
-#grade = soup.find_all('h3', class_="c12638") # to find grade
-# print(grade) # to find grade
+# for div_tags in div:
+#   grade = div_tags.find('h3')
+#   print(grade.string)
+#   ul_tags = div_tags.find_all('ul')
+#   for ul in ul_tags:
+#     subsection = ul.find_previous_sibling('h4')
+#     if subsection:
+#       print(subsection.string)
+#     else:
+#       print("No preceding h4 sibling found for ul.")
+#     for central_content in ul.find_all('li'):
+#       print(central_content.string)
