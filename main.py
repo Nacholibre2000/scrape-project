aa434db3a224000d66a65bce56db5731e7d4e543
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-06-26 14:32:08 +0000

    broken code - my debugging efforts

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ central_content_data = {}
 central_requirement_data = {}
 
 # Keeping track of ids
+school_id = 1
 subject_id = 1
 grade_id = 1
 subsection_id = 1
@@ -42,7 +43,6 @@ for url in urls:
   # Get page content
   response = requests.get(url)
   page_content = response.content
-  #print(page_content) #debug
 
   # Initialize BeautifulSoup for the new page
   soup = BeautifulSoup(page_content, "html.parser")
@@ -60,11 +60,12 @@ for url in urls:
   subject = subject_name.string.strip()
 
   if subject not in subject_data:
-    subject_data[subject] = subject_id
+    foreign_id_subject = school_id
+    subject_data[subject] = (subject_id, foreign_id_subject)
     subject_id += 1
 
   # Get current subject ID for use in grade_data
-  subject_id_current = subject_data[subject]
+  subject_id_current, foreign_id_subject = subject_data[subject]
 
   # for-loop for central content
   for idx, central_contents_tags in enumerate(central_contents.find_all(
@@ -72,8 +73,9 @@ for url in urls:
                                               start=1):
     grade = central_contents_tags.find('h3').string
     if grade not in grade_data:
-      grade_data[grade] = (grade_id, str(subject_id_current)
-                           )  # include subject_id_current as foreign key
+      foreign_id_grade = str(foreign_id_subject) + "-" + str(
+        subject_id_current)  # Update the foreign key here
+      grade_data[grade] = (grade_id, foreign_id_grade)
       grade_id += 1
 
     grade_id_current, foreign_id_grade = grade_data[grade]
@@ -82,22 +84,22 @@ for url in urls:
     for ul in ul_tags:
       subsection = ul.find_previous_sibling('h4')
       if subsection is None:
-          subsection = "Default Subsection"  # replace this with an appropriate default string for your context
+        subsection = "Default Subsection"  # replace this with an appropriate default string for your context
       else:
-          subsection = subsection.string if subsection.string is not None else "Default Subsection"
-          # append grade if the subsection has been seen before
-          if subsection in original_subsections:
-              print(f"subsection: {subsection}") # debug
-              print(f"grade: {grade}")  # debug
-              subsection = subsection + " (" + grade + ")"
-          else:
-              original_subsections.append(subsection)
-      
-          if subsection not in subsection_data:
-              foreign_id_subsection = foreign_id_grade + "-" + str(
-                grade_id_current)  # Update the foreign key here
-              subsection_data[subsection] = (subsection_id, foreign_id_subsection)
-              subsection_id += 1
+        subsection = subsection.string if subsection.string is not None else "Default Subsection"
+        # append grade if the subsection has been seen before
+        # if subsection in original_subsections:
+        #     print(f"subsection: {subsection}") # debug
+        #     print(f"grade: {grade}")  # debug
+        #     subsection = subsection + " (" + grade + ")"
+        # else:
+        #     original_subsections.append(subsection)
+
+        if subsection not in subsection_data:
+          foreign_id_subsection = foreign_id_grade + "-" + str(
+            grade_id_current)  # Update the foreign key here
+          subsection_data[subsection] = (subsection_id, foreign_id_subsection)
+          subsection_id += 1
 
       subsection_id_current, foreign_id_subsection = subsection_data[
         subsection]
@@ -112,58 +114,59 @@ for url in urls:
           central_content_id += 1
 
   # for-loop for central requirement
-  for idx, articles in enumerate(course_requirements.find_all('article'),
-                                 start=1):
-    grade = articles.find('h3').string
+  # for idx, articles in enumerate(course_requirements.find_all('article'),
+  #                                start=1):
+  #   grade = articles.find('h3').string
 
-    if grade == "Kriterier för bedömning av godtagbara kunskaper i slutet av årskurs 3":
-      grade = "I årskurs 1–3"
+  #   if grade == "Kriterier för bedömning av godtagbara kunskaper i slutet av årskurs 3":
+  #     grade = "I årskurs 1–3"
 
-    if grade == "Betygskriterier för slutet av årskurs 6":
-      grade = "I årskurs 4–6"
+  #   if grade == "Betygskriterier för slutet av årskurs 6":
+  #     grade = "I årskurs 4–6"
 
-    if grade == "Betygskriterier för slutet av årskurs 9":
-      grade = "I årskurs 7–9"
+  #   if grade == "Betygskriterier för slutet av årskurs 9":
+  #     grade = "I årskurs 7–9"
 
-    foreign_id_grade = str(subject_id_current) + "-" + str(grade_id_current)
+  #   #foreign_id_grade = str(subject_id_current) + "-" + str(grade_id_current)
 
-    if grade not in grade_data:
-      grade_data[grade] = (grade_id, foreign_id_grade
-                           )  # include foreign_id_grade as foreign key
-      grade_id += 1
+  #   if grade not in grade_data:
+  #     foreign_id_grade = str(foreign_id_subject) + "-" + str(
+  #       subject_id_current)  # Update the foreign key here
+  #     grade_data[grade] = (grade_id, foreign_id_grade)
+  #     grade_id += 1
 
-    grade_id_current, foreign_id_grade = grade_data[grade]
+  #   grade_id_current, foreign_id_grade = grade_data[grade]
+                                   
+  #   paragraphs = articles.find('div', class_="course-details").find_all('p')
+  #   for paragraph in paragraphs:
+  #     sentences = paragraph.get_text().split('. ')
+  #     for central_requirement in sentences:
+  #       # append grade if the sentence has been seen before
+  #       if central_requirement in original_sentences:
+  #         central_requirement = central_requirement + "-" + grade
+  #       else:
+  #         original_sentences.append(central_requirement)
 
-    paragraphs = articles.find('div', class_="course-details").find_all('p')
-    for paragraph in paragraphs:
-      sentences = paragraph.get_text().split('. ')
-      for central_requirement in sentences:
-        # append grade if the sentence has been seen before
-        if central_requirement in original_sentences:
-          central_requirement = central_requirement + "-" + grade
-        else:
-          original_sentences.append(central_requirement)
+  #       foreign_id_requirement = foreign_id_grade + "-" + str(
+  #         grade_id_current)  # Foreign key is grade foreign ID + grade ID
 
-        foreign_id_requirement = foreign_id_grade + "-" + str(
-          grade_id_current)  # Foreign key is grade foreign ID + grade ID
-
-        if central_requirement not in central_requirement_data:
-          central_requirement_data[central_requirement] = (
-            central_requirement_id, foreign_id_requirement)
-          central_requirement_id += 1
+  #       if central_requirement not in central_requirement_data:
+  #         central_requirement_data[central_requirement] = (
+  #           central_requirement_id, foreign_id_requirement)
+  #         central_requirement_id += 1
 
 # Create and write to CSV files
 with open('subject_data.csv', 'w', newline='') as file:
   writer = csv.writer(file)
-  writer.writerow(["id", "subject"])
+  writer.writerow(["id", "subject", "school_id"])
   for key, value in subject_data.items():
-    writer.writerow([value, key])
+    writer.writerow([value[0], key, value[1]])
 
 with open('grade_data.csv', 'w', newline='') as file:
   writer = csv.writer(file)
   writer.writerow(["id", "grade",
                    "foreign_id_subject"])  # add foreign_id_subject
-  for key, value in subsection_data.items():
+  for key, value in grade_data.items():
     writer.writerow([value[0], key, value[1]])
 
 with open('subsection_data.csv', 'w', newline='') as file:
@@ -178,8 +181,8 @@ with open('central_content_data.csv', 'w', newline='') as file:
   for key, value in central_content_data.items():
     writer.writerow([value[0], key, value[1]])
 
-with open('central_requirement_data.csv', 'w', newline='') as file:
-  writer = csv.writer(file)
-  writer.writerow(["id", "central_requirement", "foreign_id_grades"])
-  for key, value in central_requirement_data.items():
-    writer.writerow([value[0], key, value[1]])
+# with open('central_requirement_data.csv', 'w', newline='') as file:
+#   writer = csv.writer(file)
+#   writer.writerow(["id", "central_requirement", "foreign_id_grades"])
+#   for key, value in central_requirement_data.items():
+#     writer.writerow([value[0], key, value[1]])
